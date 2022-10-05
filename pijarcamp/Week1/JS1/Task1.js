5c3da883832d559fc3e49c8ea96b384fda276927
Fanani23 <pramudiasf23@gmail.com>
2022-10-05 13:00:58 +0700

    update task 1

---
+++ pijarcamp/Week1/JS1/Task1.js
@@ -0,0 +1,51 @@
+// variabel name
+let name = 'Pramudia Syahrul Fanani';
+
+// variable address
+let address = {
+    Jalan : 'Sarean',
+    RT : 02,
+    RW : 01,
+    Dusun : 'Gondoruso',
+    Desa : 'Japanan',
+    Kecamatan : 'Kemlagi',
+    Kabupaten : 'Mojokerto',
+    Provinsi : 'Jawa Timur',
+    kodePos : 61353
+};
+
+// variable email
+let email = 'pramudiasf23@gmail.com';
+
+// variable hoby
+let hobbies = [
+    'Membaca',
+    'Menulis',
+    'Bermain gitar',
+    'Basket',
+    'Badminton',
+    'Mendengarkan musik'
+];
+
+// variable age
+let age = 22;
+
+// variable is merried
+let isMerried = false;
+
+// check data type
+console.log(
+    '\n' +
+    'Data type of name is ' + typeof(name),
+    '\n' +
+    'Data type of address is ' + typeof(address),
+    '\n' +
+    'Data type of email is ' + typeof(email),
+    '\n' +
+    'Data type of hobbies is ' + typeof(hobbies),
+    '\n' +
+    'Data type of age is ' + typeof(age),
+    '\n' +
+    'Data type of isMerried is ' + typeof(isMerried),
+    '\n'
+);
