d09ba52958d71fb2866748d27b8a9968d0a1cd90
Fanani23 <pramudiasf23@gmail.com>
2022-10-05 14:37:25 +0700

    update task 3

---
+++ pijarcamp/Week1/JS1/Task3.js
@@ -0,0 +1,26 @@
+// readline declaration
+var readLine = require('readline');
+
+// rl declaration for readline function
+var rl = readLine.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+// variable and input declaration
+rl.question('Masukkan angka : ', (userInput) => {
+    // variable declaration
+    let printSegitiga = userInput;
+    // check, if input value is not number
+    // check, if input value is not minus
+    if (!isNaN(printSegitiga) && printSegitiga>=0) {
+        for (let i=1; i<=printSegitiga; i++) {
+            let str = [];
+            for (let j=1; j<=i; j++) {
+                str.push(i);
+            } console.log(str.join(""));
+        };
+        // close
+        rl.close();
+    };
+});
