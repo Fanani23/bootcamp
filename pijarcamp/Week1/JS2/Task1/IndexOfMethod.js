a9b1a495cb901fa465234a9808c4e6d84901623e
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:13:00 +0700

    upload task 1 - indexOf method

---
+++ pijarcamp/Week1/JS2/Task1/IndexOfMethod.js
@@ -0,0 +1,13 @@
+// Method indexOf
+/*
+Digunakan untuk mencari letak sebuah index dalam array.
+Jika index yang dicari ada -> index ke-n
+Jika tidak ada -> -1
+*/
+// Syntax
+// array.indexOf('index');
+let contohIndexOf = ['Pramudia', 'Marcheilla', 'Dy', 'Sherlyn'];
+console.log(contohIndexOf);
+console.log(contohIndexOf.indexOf('Pramudia'));
+console.log(contohIndexOf.indexOf('Marcheilla'));
+console.log(contohIndexOf.indexOf('Elsa'));
