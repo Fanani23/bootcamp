515f4787005481571727bbad05608b209a5d4028
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:08:06 +0700

    upload task 1 - push method

---
+++ pijarcamp/Week1/JS2/Task1/PushMethod.js
@@ -0,0 +1,12 @@
+// Method push
+/*
+Digunakan untuk menambah satu atau lebih data baru,
+di akhir sebuah array.
+*/
+// Syntax
+// array.push (a1, a2, ..., an);
+// Contoh penggunaan
+let contohPush = ['Pramudia', 'Marcheilla', 'Dy'];
+console.log(contohPush);
+contohPush.push('Sherlyn');
+console.log(contohPush);
