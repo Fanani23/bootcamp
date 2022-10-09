f135bb5e82267e1af9fa6fa53ae996db9056b7cd
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:11:51 +0700

    upload task 1 - unshift method

---
+++ pijarcamp/Week1/JS2/Task1/UnshiftMethod.js
@@ -0,0 +1,11 @@
+// Method unshift
+/*
+Digunakan untuk menambahkan index baru di awal sebuah array.
+*/
+// Syntax
+// array.unshift(a1, a2, ..., an);
+// Contoh penggunaan
+let contohUnshift = ['Marcheilla', 'Dy', 'Sherlyn'];
+console.log(contohUnshift);
+contohUnshift.unshift('Pramudia');
+console.log(contohUnshift);
