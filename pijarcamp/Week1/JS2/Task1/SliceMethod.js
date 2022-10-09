5bede754285c9c135d2fa161ceba965a6be781ef
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:09:49 +0700

    upload task 1 - slice method

---
+++ pijarcamp/Week1/JS2/Task1/SliceMethod.js
@@ -0,0 +1,11 @@
+// Method slice
+/*
+Digunakan untuk memilih satu atau lebih index dari sebuah array.
+*/
+// Syntax
+// array.slice(start, end);
+// Contoh penggunaan
+let contohSlice = ['Pramudia', 'Marcheilla', 'Dy', 'Sherlyn'];
+console.log(contohSlice);
+console.log(contohSlice.slice(0, 2));
+console.log(contohSlice.slice(0, 3));
