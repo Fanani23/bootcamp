c13760793c402e67fb269610f940443050dc0a45
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:10:52 +0700

    upload task 1 - concat method

---
+++ pijarcamp/Week1/JS2/Task1/ConcatMethod.js
@@ -0,0 +1,17 @@
+// Method concat
+/*
+Digunakan untuk menggabungkan array (dua atau lebih).
+*/
+// Syntax
+// array1.concat(array2, array3, ..., arrayn);
+// Contoh penggunaan
+let contohConcat1 = ['Pramudia', 'Marcheilla'];
+let contohConcat2 = ['Dy'];
+let contohCOncat3 = ['Sherlyn'];
+
+console.log(contohConcat1);
+console.log(contohConcat2);
+console.log(contohCOncat3);
+
+let contohConcat4 = contohConcat1.concat(contohConcat2, contohCOncat3);
+console.log(contohConcat4);
