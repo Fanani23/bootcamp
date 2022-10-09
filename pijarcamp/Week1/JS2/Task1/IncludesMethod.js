214000d2bbb9b22f2d84f664c7290dd22ce3a19f
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:12:25 +0700

    upload task 1 - includes method

---
+++ pijarcamp/Week1/JS2/Task1/IncludesMethod.js
@@ -0,0 +1,15 @@
+// Method includes
+/*
+Digunakan untuk element tertentu dari sebuah array.
+Jika index ada -> true,
+Jika tidak ada -> false.
+*/
+// Syntax
+// array.includes('index');
+// Contoh penggunaan
+let contohIncludes = ['Pramudia', 'Marcheilla', 'Dy', 'Sherlyn'];
+console.log(contohIncludes);
+// True
+console.log(contohIncludes.includes('Pramudia'));
+// False
+console.log(contohIncludes.includes('Elsa'));
