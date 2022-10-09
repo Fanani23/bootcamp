4ab2372b8e77a3a29abe5aa58a1b75de6cc4e45b
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:08:49 +0700

    upload task 1 - pop method

---
+++ pijarcamp/Week1/JS2/Task1/PopMethod.js
@@ -0,0 +1,11 @@
+// Method pop
+/*
+Digunakan untuk menghapus item terakhir dari sebuah array.
+*/
+// Syntax
+// array.pop();
+// Contoh penggunaan
+let contohPop = ['Pramudia', 'Marcheilla', 'Dy', 'Sherlyn'];
+console.log(contohPop);
+contohPop.pop();
+console.log(contohPop);
