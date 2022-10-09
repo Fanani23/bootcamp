8a2efacb3342f6249a2fe74036b2d6bf836bc800
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:09:20 +0700

    upload task 1 - shift method

---
+++ pijarcamp/Week1/JS2/Task1/ShiftMethod.js
@@ -0,0 +1,11 @@
+// Shift
+/*
+Digunakan untuk menghapus index pertama dari sebuah array.
+*/
+// Syntax
+// array.shift();
+// Contoh penggunaan
+let contohShift = ['Pramudia', 'Marcheilla', 'Dy', 'Sherlyn'];
+console.log(contohShift);
+contohShift.shift();
+console.log(contohShift);
