0395c9b98cc230f71da9933c43701989537d2821
Fanani23 <pramudiasf23@gmail.com>
2022-10-09 19:13:32 +0700

    upload task 1 - sort method

---
+++ pijarcamp/Week1/JS2/Task1/SortMethod.js
@@ -0,0 +1,16 @@
+// Method sort
+/*
+Digunakan untuk mengurutkan index array,
+secara ascending maupun descending.
+*/
+// Syntax
+// array.sort();
+// Contoh penggunaan
+let contohSort = ['Pramudia', 'Marcheilla', 'Dy', 'Sherlyn', 'Elsa'];
+console.log(contohSort);
+// ascending
+contohSort.sort();
+console.log(contohSort);
+// descending
+contohSort.reverse();
+console.log(contohSort);
