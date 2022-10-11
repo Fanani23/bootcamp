220be46d7920ee6275d586b90b537f5a05e8ba26
Fanani23 <pramudiasf23@gmail.com>
2022-10-11 22:23:18 +0700

    upload task 1 - try catch

---
+++ pijarcamp/Week2/JS3/Task1/Tc.js
@@ -0,0 +1,22 @@
+const cekHariKerja = (day) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dataDay = ['Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at"];
+            let cek = dataDay.find((item) => {
+                return item === day
+            })
+            if (cek) {
+                resolve(cek + ' adalah hari kerja.')
+            } else {
+                reject(new Error('Hari ini bukan hari kerja!'))
+            }
+        }, 3000)
+    })
+}
+
+try {
+    let result = cekHariKerja('SeSabtu');
+    console.log(result);
+} catch(err){
+    console.log({message: err.message});
+} 
