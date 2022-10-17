8b7a5da54b9dbac00b35c8b3c8147ebb8c08fe6a
Fanani23 <pramudiasf23@gmail.com>
2022-10-17 11:31:26 +0700

    update task 2 week 2 CI

--- pijarcamp/Week2/CI/Task2.js
+++ pijarcamp/Week2/CI/Task2.js
@@ -4,12 +4,12 @@ const pijarcamp = (price, voucher, distance, tax) => {
         if (discountAmmount > 50000) {
             discountAmmount = 50000;
         }
-        console.log('Harga : ', price);
-        console.log('Potongan :', discountAmmount); 
+        console.log('Harga          : ', price);
+        console.log('Potongan       : ', discountAmmount); 
 
-        if (distance === 2) {
+        if (distance <= 2) {
             var distanceAmmountTotal = 5000;
-            console.log('Biaya antar : ', distanceAmmountTotal);
+            console.log('Biaya antar    : ', distanceAmmountTotal);
         } else if (distance > 2) {
             var str = 0;
             for (let i=2; i<distance; i++) {
@@ -17,22 +17,22 @@ const pijarcamp = (price, voucher, distance, tax) => {
             }
             var distanceAmmount = 5000;
             var distanceAmmountTotal = distanceAmmount + str;
-            console.log('Biaya antar : ', distanceAmmountTotal);
+            console.log('Biaya antar    : ', distanceAmmountTotal);
         } else {
             var distanceZero = 0;
-            console.log('Biaya antar : ', distanceZero);
+            console.log('Biaya antar    : ', distanceZero);
         }
         
         if (tax != true) {
             var taxZero = 0;
-            console.log('Pajak : ', taxZero);
+            console.log('Pajak          : ', taxZero);
         } else {
             var taxAmmount = price * (5 / 100);
-            console.log('Pajak : ', taxAmmount);
+            console.log('Pajak          : ', taxAmmount);
         }
 
         let subTotal = discountAmmount+distanceAmmountTotal+taxAmmount;
-        console.log('Sub total : ', subTotal);
+        console.log('Sub total      : ', subTotal);
         
 
     } else if (voucher == "DITRAKTIRPIJAR" && price >= 25000) {
@@ -40,12 +40,12 @@ const pijarcamp = (price, voucher, distance, tax) => {
         if (discountAmmount > 30000) {
             discountAmmount = 30000;
         }
-        console.log('Harga : ', price);
-        console.log('Potongan : ', discountAmmount);
+        console.log('Harga          : ', price);
+        console.log('Potongan       : ', discountAmmount);
 
-        if (distance === 2) {
+        if (distance <= 2) {
             var distanceAmmountTotal = 5000;
-            console.log('Biaya antar : ', distanceAmmountTotal);
+            console.log('Biaya antar    : ', distanceAmmountTotal);
         } else if (distance > 2) {
             var str = 0;
             for (let i=2; i<distance; i++) {
@@ -53,29 +53,29 @@ const pijarcamp = (price, voucher, distance, tax) => {
             }
             var distanceAmmount = 5000;
             var distanceAmmountTotal = distanceAmmount + str;
-            console.log('Biaya antar : ', distanceAmmountTotal);
+            console.log('Biaya antar    : ', distanceAmmountTotal);
         } else {
             var distanceZero = 0;
-            console.log('Biaya antar : ', distanceZero);
+            console.log('Biaya antar    : ', distanceZero);
         }
 
         if (tax != true) {
             var taxZero = 0;
-            console.log('Pajak : ', taxZero);
+            console.log('Pajak          : ', taxZero);
         } else {
             var taxAmmount = price * (5 / 100);
-            console.log('Pajak : ', taxAmmount);
+            console.log('Pajak          : ', taxAmmount);
         }
 
         let subTotal = discountAmmount+distanceAmmountTotal+taxAmmount;
-        console.log('Sub total : ', subTotal);
+        console.log('Sub total      : ', subTotal);
 
     } else {
         if (voucher == "") {
-            console.log('Harga : ', price);
-            if (distance === 2) {
+            console.log('Harga          : ', price);
+            if (distance <= 2) {
                 var distanceAmmountTotal = 5000;
-                console.log('Biaya antar : ', distanceAmmountTotal);
+                console.log('Biaya antar    : ', distanceAmmountTotal);
             } else if (distance > 2) {
                 var str = 0;
                 for (let i=2; i<distance; i++) {
@@ -83,25 +83,28 @@ const pijarcamp = (price, voucher, distance, tax) => {
                 }
                 var distanceAmmount = 5000;
                 var distanceAmmountTotal = distanceAmmount + str;
-                console.log('Biaya antar : ', distanceAmmountTotal);
+                console.log('Biaya antar    : ', distanceAmmountTotal);
             } else {
                 var distanceZero = 0;
-                console.log('Biaya antar : ', distanceZero);
+                console.log('Biaya antar    : ', distanceZero);
             }
             
             if (tax != true) {
                 var taxZero = 0;
-                console.log('Pajak : ', taxZero);
+                console.log('Pajak          : ', taxZero);
             } else {
                 var taxAmmount = price * (5 / 100);
-                console.log('Pajak : ', taxAmmount);
+                console.log('Pajak          : ', taxAmmount);
             }
     
             let subTotal = price-(distanceAmmountTotal+taxAmmount);
-            console.log('Sub total : ', subTotal);
+            console.log('Sub total      : ', subTotal);
         }
     }
 }
 
-// pijarcamp(75000, "PIJARFOOD5", 5, true)
-pijarcamp(30000, 'DITRAKTIRPIJAR', 5, true)
+pijarcamp(75000, "PIJARFOOD5", 1, true)
+console.log("....")
+pijarcamp(30000, 'DITRAKTIRPIJAR', 1, true)
+console.log("....")
+pijarcamp(70000, '', 1, true)
